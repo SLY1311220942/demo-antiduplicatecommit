2cc640f4d6c013dcef32ed2482108d14fe15b27c
盛领宜 <sheng.ly_wh@www.sand.com>
2019-07-12 14:11:58 +0800

    提交

--- src/main/java/com/sly/demo/antiduplicatecommit/controller/EmailController.java
+++ src/main/java/com/sly/demo/antiduplicatecommit/controller/EmailController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.commons.mail.EmailAttachment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +31,7 @@ public class EmailController {
 	private EmailSender emailSender;
 	
 	/**
-	 * fa
+	 * 
 	 * @param request
 	 * @param response
 	 * @return
@@ -43,11 +44,119 @@ public class EmailController {
 		BaseResult result = new BaseResult();
 		try {
 			MailInfo mailInfo = new MailInfo();
-			mailInfo.addAddressee("1311220942@qq.com");
+			
 			mailInfo.setContent("sendSimpleEmail测试内容");
 			mailInfo.setSubject("sendSimpleEmail测试");
+			mailInfo.addAddressee("1311220942@qq.com");
 			
 			emailSender.sendSimpleEmail(mailInfo);
+			
+			result.setStatus(ResultStatus.SUCCESS);
+		} catch (Exception e) {
+			LOGGER.error(ExceptionUtils.getStackTrace(e));
+			result.setStatus(ResultStatus.FAILED);
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * 
+	 * @param request
+	 * @param response
+	 * @return
+	 * @author sly
+	 * @time 2019年7月11日
+	 */
+	@ResponseBody
+	@RequestMapping("/sendMultiPartEmail")
+	public BaseResult sendMultiPartEmail(HttpServletRequest request,HttpServletResponse response) {
+		BaseResult result = new BaseResult();
+		try {
+			EmailAttachment attachment = new EmailAttachment();
+			attachment.setName("测试文件.txt");
+			attachment.setPath("D:\\test\\ftpdownload.txt");
+			
+			MailInfo mailInfo = new MailInfo();
+			
+			mailInfo.setContent("sendMultiPartEmail测试内容");
+			mailInfo.setSubject("sendMultiPartEmail测试");
+			mailInfo.addAddressee("1311220942@qq.com");
+			mailInfo.addAttachment(attachment);
+			
+			emailSender.sendMultiPartEmail(mailInfo);
+			
+			result.setStatus(ResultStatus.SUCCESS);
+		} catch (Exception e) {
+			LOGGER.error(ExceptionUtils.getStackTrace(e));
+			result.setStatus(ResultStatus.FAILED);
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * 
+	 * @param request
+	 * @param response
+	 * @return
+	 * @author sly
+	 * @time 2019年7月11日
+	 */
+	@ResponseBody
+	@RequestMapping("/sendHtmlEmail")
+	public BaseResult sendHtmlEmail(HttpServletRequest request,HttpServletResponse response) {
+		BaseResult result = new BaseResult();
+		try {
+			EmailAttachment attachment = new EmailAttachment();
+			attachment.setName("测试文件.txt");
+			attachment.setPath("D:\\test\\ftpdownload.txt");
+			
+			MailInfo mailInfo = new MailInfo();
+			
+			mailInfo.setContent("<h1>sendHtmlEmail测试内容</h1>");
+			mailInfo.setSubject("sendSimpleEmail测试");
+			mailInfo.addAddressee("1311220942@qq.com");
+			mailInfo.addAttachment(attachment);
+			
+			emailSender.sendHtmlEmail(mailInfo);
+			
+			result.setStatus(ResultStatus.SUCCESS);
+		} catch (Exception e) {
+			LOGGER.error(ExceptionUtils.getStackTrace(e));
+			result.setStatus(ResultStatus.FAILED);
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * 
+	 * @param request
+	 * @param response
+	 * @return
+	 * @author sly
+	 * @time 2019年7月11日
+	 */
+	@ResponseBody
+	@RequestMapping("/sendImageHtmlEmail")
+	public BaseResult sendImageHtmlEmail(HttpServletRequest request,HttpServletResponse response) {
+		BaseResult result = new BaseResult();
+		try {
+			EmailAttachment attachment = new EmailAttachment();
+			attachment.setName("测试文件.txt");
+			attachment.setPath("D:\\test\\ftpdownload.txt");
+			
+			MailInfo mailInfo = new MailInfo();
+			
+			
+			mailInfo.setContent("<h1>sendImageHtmlEmail测试内容</h1>" + "<img src=\"D:\\test\\image.jpg\">"
+					+ " <img src=\"http://commons.apache.org/proper/commons-email/images/commons-logo.png\">");
+			mailInfo.setSubject("sendSimpleEmail测试");
+			mailInfo.addAddressee("1311220942@qq.com");
+			mailInfo.addAttachment(attachment);
+			
+			emailSender.sendImageHtmlEmail(mailInfo);
 			
 			result.setStatus(ResultStatus.SUCCESS);
 		} catch (Exception e) {
